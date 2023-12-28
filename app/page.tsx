0da441eda21379abc4dec42e1282f2260cc90754
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-28 21:11:35 +0100

    Create header with dropbox logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { UserButton } from '@clerk/nextjs';
-
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox Clone</h1>
-
-      <UserButton />
     </main>
   )
 }
