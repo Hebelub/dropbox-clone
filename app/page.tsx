4dd37fa6d2c39f5ef2253d8665fb313eb6a2436d
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-28 22:05:20 +0100

    Add content to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="">
-      <h1 className="">Dropbox Clone</h1>
+      <div>
+        <div className="p-10 flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox <br />
+            <br />
+            Storing everything for you and your buisness needs. All in one place.
+          </h1>
+
+          <p className="pb-20">
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize, and access files from anywhere.
+            Securely store important documents and media, and experience the
+            convenience of easy file management and sharing in one centralized
+            solution.
+          </p>
+
+          <Link
+            href="/dashboard"
+            className="flex cursor-pointer bg-blue-500 p-5 w-fit"
+          >
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+      </div>
+
+      <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
+      <p className="text-center font-light p-2">
+        This Dropbox clone is made for educational purposes only. I do not own or affiliate with Dropbox or/and any of its subsidiaries in any form. Copyright Disclaimer under section 107 of the Copyright Act 1976, allowance is made for "fair use" of this video for educational purposes.
+      </p>
     </main>
   )
 }
