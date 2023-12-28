7dc6fc0a51d0bd527719d87c6dc568ccb0a74989
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-28 20:44:22 +0100

    Setup google login with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox Clone</h1>
+
+      <UserButton />
     </main>
   )
 }
