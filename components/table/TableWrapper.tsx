d3b9a915a9f3995bf4e5835e33e6df7a6079bcf2
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-29 23:53:10 +0100

    Added sorting

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -4,18 +4,53 @@ import { FileType } from "@/typings";
 import { Button } from "../ui/button";
 import { DataTable } from "./Table";
 import { columns } from "./columns";
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
-  return (
-    <div>
-        <Button>Sort By ...</Button>
+    const { isLoaded, isSignedIn, user } = useUser();
+    const [initalFiles, setInitialFiles] = useState<FileType[]>([]);
+    const [sort, setSort] = useState<"asc" | "desc">("asc");
 
-        <DataTable
-            columns={columns}
-            data={skeletonFiles}
-        />
-    </div>
-  )
+    const [docs, loading, error] = useCollection(
+        user && 
+            query(
+                collection(db, "users", user.id, "files"),
+                orderBy("timestamp", sort)
+            )
+    );
+
+    useEffect(() => {
+        if (!docs) return;
+
+        const files: FileType[] = docs.docs.map(doc => ({
+            id: doc.id,
+            filename: doc.data().filename || doc.id,
+            timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+            fullName: doc.data().fullName,
+            downloadURL: doc.data().downloadURL,
+            type: doc.data().type,
+            size: doc.data().size,
+        }));
+
+        setInitialFiles(files);
+    }, [docs])
+
+    return (
+        <div>
+            <Button onClick={() => setSort(sort === "desc" ? "asc" : "desc")}>
+                Sort By {sort === "desc" ? "Newest" : "Oldest"}
+            </Button>
+
+            <DataTable
+                columns={columns}
+                data={initalFiles}
+            />
+        </div>
+    )
 }
 
 export default TableWrapper
