29489d39b486af68cbabc6238573942f6e59d26a
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-30 11:22:18 +0100

    Add delete functionallity

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -18,6 +18,8 @@ import {
 import { FileType } from "@/typings"
 import { Button } from "../ui/button"
 import { PencilIcon, TrashIcon } from "lucide-react"
+import { useAppStore } from "@/store/store"
+import { DeleteModal } from "../DeleteModal"
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -33,6 +35,26 @@ export function DataTable<TData, TValue>({
         columns,
         getCoreRowModel: getCoreRowModel(),
     })
+
+    const [setIsDeleteModalOpen, setFileId, setFilename, setIsRenameModalOpen,
+    ] = useAppStore((state) => [
+        state.setIsDeleteModalOpen,
+        state.setFileId,
+        state.setFilename,
+        state.setIsRenameModalOpen,
+    ]);
+
+    const openDeleteModal = (fileId: string) => {
+        setFileId(fileId);
+        setIsDeleteModalOpen(true);
+    };
+
+    const openRenameModal = (fileId: string, filename: string) => {
+        setFileId(fileId);
+        setFilename(filename);
+        setIsRenameModalOpen(true);
+    };
+
 
     return (
         <div className="rounded-md border">
@@ -62,6 +84,8 @@ export function DataTable<TData, TValue>({
                                 key={row.id}
                                 data-state={row.getIsSelected() && "selected"}
                             >
+                                <DeleteModal />
+                                
                                 {row.getVisibleCells().map((cell) => (
                                     <TableCell key={cell.id}>
                                         {cell.column.id === "timestamp" ? (
@@ -76,11 +100,10 @@ export function DataTable<TData, TValue>({
                                         ) : cell.column.id === 'filename' ? (
                                             <p
                                                 onClick={() => {
-                                                    console.log("hello");
-                                                    // openRenameModal(
-                                                    //   (row.original as FileType).id,
-                                                    //   (row.original as FileType).filename
-                                                    // )
+                                                    openRenameModal(
+                                                      (row.original as FileType).id,
+                                                      (row.original as FileType).filename
+                                                    )
                                                 }}
                                                 className="underline flex items-center text-blue-500 hover:cursor-pointer"
                                             >
@@ -98,8 +121,7 @@ export function DataTable<TData, TValue>({
                                     <Button
                                         variant={"outline"}
                                         onClick={() => {
-                                            console.log("Opening delete modal");
-                                            // openDeleteModal((row.original as FileType).id))
+                                            openDeleteModal((row.original as FileType).id)
                                         }}
                                     >
                                         <TrashIcon size={20} />
